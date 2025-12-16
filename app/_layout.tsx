890b97135b3022fae8f04553274ddbff62be81a8
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-12-16 09:35:16 +0100

    update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -44,6 +44,8 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="register" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
