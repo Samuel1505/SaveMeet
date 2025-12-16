5e59b1495033f46d116ad31662f314b70ded0230
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-12-16 09:47:10 +0100

    Update:update supabaselib with secret keys

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,9 @@
-import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'process.env.SUPABASE_URL';
+const supabaseAnonKey = 'process.env.SUPABASE_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
